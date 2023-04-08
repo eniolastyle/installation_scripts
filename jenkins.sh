860fc029dea4be72310e255d480db04ea434121e
eniolastyle <amiolastyle@gmail.com>
2023-04-08 13:20:23 +0100

    jenkins installation updated

--- jenkins.sh
+++ jenkins.sh
@@ -8,15 +8,27 @@ java -version
 
 sudo apt update -y
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-sudo apt update -y
+#sudo apt update -y
 
-sudo add-apt-repository universe -y
+#sudo add-apt-repository universe -y
 
-sudo apt-get install jenkins -y
+#sudo apt-get install jenkins -y
+
+sudo apt install ca-certificates
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install jenkins
 
 sudo service jenkins start
 
